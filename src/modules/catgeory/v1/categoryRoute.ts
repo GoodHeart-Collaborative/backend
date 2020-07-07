4eab484f3a8b905bb644998b45febbe523f6e344
shubham.maheshwari@appinventiv.com <shubham.maheshwari@appinventiv.com>
2020-07-07 15:15:13 +0530

    category

--- src/modules/catgeory/v1/categoryRoute.ts
+++ src/modules/catgeory/v1/categoryRoute.ts
@@ -35,7 +35,7 @@ export const categoryRoute: ServerRoute[] = [
                 payload: {
                     // name: Joi.string().lowercase().required(),
                     title: Joi.string().required(),
-                    imageUrl: Joi.strig()
+                    imageUrl: Joi.string()
                 },
                 failAction: appUtils.failActionFunction
             },
