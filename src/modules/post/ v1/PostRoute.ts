de949116d866fe0f52d7179c16936b548727ec84
shubham.maheshwari@appinventiv.com <shubham.maheshwari@appinventiv.com>
2020-07-08 17:35:36 +0530

    update

--- src/modules/post/ v1/PostRoute.ts
+++ src/modules/post/ v1/PostRoute.ts
@@ -8,83 +8,35 @@ import * as appUtils from "@utils/appUtils";
 import * as validator from "@utils/validator";
 import * as config from "@config/index";
 import { responseHandler } from "@utils/ResponseHandler";
+import { join } from "path";
 
 export const postRoute: ServerRoute[] = [
-    {
-        method: "POST",
-        path: `${config.SERVER.API_BASE_URL}/v1/admin/post`,
-        handler: async (request: Request, h: ResponseToolkit) => {
-            const tokenData: TokenData = request.auth && request.auth.credentials && request.auth.credentials.tokenData.adminData;
-            const payload = request.payload;
-            try {
-                appUtils.consolelog("This request is on", `${request.path}with parameters ${JSON.stringify(payload)}`, true);
-                const result = await postController.addPost(payload);
-                return responseHandler.sendSuccess(h, result);
-            } catch (error) {
-                return responseHandler.sendError(error);
-            }
-        },
-        config: {
-            tags: ["api", "posts"],
-            description: "Add post",
-            auth: {
-                strategies: ["AdminAuth"]
-            },
-            validate: {
-                headers: validator.adminAuthorizationHeaderObj,
-                payload: {
-                    userId: Joi.string().optinal(), // 
-                    categoryId: Joi.string().required(),
-                    // subCategoryId: Joi.string().required(),
-                    title: Joi.string(),
-                    privacy: Joi.string().valid([
-                        config.CONSTANT.PRIVACY_STATUS.PUBLIC,
-                        config.CONSTANT.PRIVACY_STATUS.PROTECTED,
-                        config.CONSTANT.PRIVACY_STATUS.PRIVATE
-                    ]),
-                    description: Joi.string(),
-                    // shortDescription: string;
-                    imageUrl: Joi.string(),
-                },
-                failAction: appUtils.failActionFunction
-            },
-            plugins: {
-                "hapi-swagger": {
-                    // payloadType: 'form',
-                    responseMessages: config.CONSTANT.SWAGGER_DEFAULT_RESPONSE_MESSAGES
-                }
-            }
-        }
-    },
-
     // {
     //     method: "POST",
     //     path: `${config.SERVER.API_BASE_URL}/v1/admin/post`,
     //     handler: async (request: Request, h: ResponseToolkit) => {
-    //         const headers: Device = request.headers;
+    //         const tokenData: TokenData = request.auth && request.auth.credentials && request.auth.credentials.tokenData.adminData;
     //         const payload = request.payload;
     //         try {
-    //             console.log('payloadpayloadpayload', payload);
-
-    //             const result = await postController.addPost({ ...headers, ...payload });
+    //             appUtils.consolelog("This request is on", `${request.path}with parameters ${JSON.stringify(payload)}`, true);
+    //             const result = await postController.addPost(payload);
     //             return responseHandler.sendSuccess(h, result);
     //         } catch (error) {
     //             return responseHandler.sendError(error);
     //         }
     //     },
-    //     options: {
+    //     config: {
     //         tags: ["api", "posts"],
-    //         description: "admin add posts",
-    //         // notes: "",
+    //         description: "Add post",
     //         auth: {
     //             strategies: ["AdminAuth"]
     //         },
     //         validate: {
-    //             headers: validator.adminAuthorizationHeaderObj["required"],
+    //             headers: validator.adminAuthorizationHeaderObj,
     //             payload: {
-    //                 userId: Joi.string().required(), // 
+    //                 userId: Joi.string().optional(), // 
     //                 categoryId: Joi.string().required(),
-    //                 subCategoryId: Joi.string().required(),
+    //                 // subCategoryId: Joi.string().required(),
     //                 title: Joi.string(),
     //                 privacy: Joi.string().valid([
     //                     config.CONSTANT.PRIVACY_STATUS.PUBLIC,
@@ -105,4 +57,78 @@ export const postRoute: ServerRoute[] = [
     //         }
     //     }
     // },
+    // {
+    //     method: "GET",
+    //     path: `${config.SERVER.API_BASE_URL}/v1/admin/post/{postId}`,
+    //     handler: async (request: Request, h: ResponseToolkit) => {
+    //         const tokenData: TokenData = request.auth && request.auth.credentials && request.auth.credentials.tokenData.adminData;
+    //         const payload = request.params;
+    //         try {
+    //             appUtils.consolelog("This request is on", `${request.path}with parameters ${JSON.stringify(payload)}`, true);
+    //             const result = await postController.getPostById(payload);
+    //             return responseHandler.sendSuccess(h, result);
+    //         } catch (error) {
+    //             return responseHandler.sendError(error);
+    //         }
+    //     },
+    //     config: {
+    //         tags: ["api", "posts"],
+    //         description: "Add post",
+    //         auth: {
+    //             strategies: ["AdminAuth"]
+    //         },
+    //         validate: {
+    //             headers: validator.adminAuthorizationHeaderObj,
+    //             params: {
+    //                 userId: Joi.string().optional(), // 
+    //             },
+    //             failAction: appUtils.failActionFunction
+    //         },
+    //         plugins: {
+    //             "hapi-swagger": {
+    //                 // payloadType: 'form',
+    //                 responseMessages: config.CONSTANT.SWAGGER_DEFAULT_RESPONSE_MESSAGES
+    //             }
+    //         }
+    //     }
+    // },
+
+
+    // {
+    //     method: "GET",
+    //     path: `${config.SERVER.API_BASE_URL}/v1/admin/post`,
+    //     handler: async (request: Request, h: ResponseToolkit) => {
+    //         const tokenData: TokenData = request.auth && request.auth.credentials && request.auth.credentials.tokenData.adminData;
+    //         const payload = request.payload;
+    //         try {
+    //             appUtils.consolelog("This request is on", `${request.path}with parameters ${JSON.stringify(payload)}`, true);
+    //             const result = await postController.getPosts(payload);
+    //             return responseHandler.sendSuccess(h, result);
+    //         } catch (error) {
+    //             return responseHandler.sendError(error);
+    //         }
+    //     },
+    //     config: {
+    //         tags: ["api", "posts"],
+    //         description: "Add post",
+    //         auth: {
+    //             strategies: ["AdminAuth"]
+    //         },
+    //         validate: {
+    //             headers: validator.adminAuthorizationHeaderObj,
+    //             payload: {
+    //                 limit: Joi.number(),
+    //                 page: Joi.number()
+    //             },
+    //             failAction: appUtils.failActionFunction
+    //         },
+    //         plugins: {
+    //             "hapi-swagger": {
+    //                 // payloadType: 'form',
+    //                 responseMessages: config.CONSTANT.SWAGGER_DEFAULT_RESPONSE_MESSAGES
+    //             }
+    //         }
+    //     }
+    // },
+
 ];
