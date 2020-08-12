1db2e0099734de2b71234293b907bc04342f2944
shubham <shubham.maheshwari@appinventiv.com>
2020-08-12 20:20:47 +0530

    update

--- src/modules/home/HomeDao.ts
+++ src/modules/home/HomeDao.ts
@@ -15,34 +15,37 @@ export class HomeDao extends BaseDao {
             let match: any = {};
             let aggPipe = [];
             let result: any = {}
-           let  endDateee = new Date();
-           endDateee.setHours(23,59,59,999);
+            let endDateee = new Date();
+            endDateee.setHours(23, 59, 59, 999);
             match["postedAt"] = { $lte: endDateee }// moment(new Date()).format('YYYY-MM-DD')
             match["status"] = config.CONSTANT.STATUS.ACTIVE
             aggPipe.push({ "$sort": { "createdAt": -1 } });
             if (endDate) {
                 match["createdAt"] = { $lt: new Date(endDate) };
             }
-            if(type) {
+            if (type) {
                 match["type"] = type
                 aggPipe.push({ "$match": match });
             } else {
                 aggPipe.push({ "$match": match });
-                aggPipe.push({ $group : { _id : "$type",  
-                description: { $first : "$description" },
-                _idd: { $first : "$_id" },
-                likeCount: { $first : "$likeCount" },
-                commentCount: { $first : "$commentCount" },
-                // status: { $first : "$status" },
-                type: { $first : "$type" },
-                mediaType: { $first : "$mediaType" },
-                created: { $first : "$created" },
-                mediaUrl: { $first : "$mediaUrl" },
-                // title: { $first : "$title" },
-                // isPostLater: { $first : "$isPostLater" },
-                // postedAt: { $first : "$postedAt" },
-                createdAt: { $first : "$createdAt" }        
-            } } )
+                aggPipe.push({
+                    $group: {
+                        _id: "$type",
+                        description: { $first: "$description" },
+                        _idd: { $first: "$_id" },
+                        likeCount: { $first: "$likeCount" },
+                        commentCount: { $first: "$commentCount" },
+                        // status: { $first : "$status" },
+                        type: { $first: "$type" },
+                        mediaType: { $first: "$mediaType" },
+                        created: { $first: "$created" },
+                        mediaUrl: { $first: "$mediaUrl" },
+                        // title: { $first : "$title" },
+                        // isPostLater: { $first : "$isPostLater" },
+                        // postedAt: { $first : "$postedAt" },
+                        createdAt: { $first: "$createdAt" }
+                    }
+                })
             }
             aggPipe.push({
                 $lookup: {
@@ -70,7 +73,7 @@ export class HomeDao extends BaseDao {
                     as: "likeData"
                 }
             })
-            aggPipe.push({ '$unwind': { path: '$likeData', preserveNullAndEmptyArrays: true } })
+            // aggPipe.push({ '$unwind': { path: '$likeData', preserveNullAndEmptyArrays: true } })
             aggPipe.push({
                 $lookup: {
                     from: "comments",
@@ -98,43 +101,44 @@ export class HomeDao extends BaseDao {
             })
             aggPipe.push({ "$addFields": { created: { "$subtract": ["$createdAt", new Date("1970-01-01")] } } });
             // aggPipe.push({ $group : { _id : "$type",  description: { $first : "$description" } } } )
-            let project:any = {
-                    // _id: "$_idd",
-                    likeCount: "$likeCount",
-                    commentCount: "$commentCount",
-                    // status: "$status",
-                    type: "$type",
-                    mediaType: "$mediaType",
-                    mediaUrl: "$mediaUrl",
-                    // thumbnailUrl: "$",
-                    // title: "$title",
-                    // createdd:"$created",
-                    // isPostLater: "$isPostLater",
-                    description: "$description",
-                    created: "$created",
-                    // postedAt: "$postedAt",
-                    createdAt: "$createdAt",
-                    isLike:
-                    {
-                        $cond: { if: { "$eq": ["$likeData.userId", await appUtils.toObjectId(userId.userId)] }, then: true, else: false }
-                    },
-                    isComment: {
-                        $cond: { if: { "$eq": [{ $size: "$commentData" }, 0] }, then: false, else: true }
-                    }
+            let project: any = {
+                // _id: "$_idd",
+                likeCount: "$likeCount",
+                commentCount: "$commentCount",
+                // status: "$status",
+                type: "$type",
+                mediaType: "$mediaType",
+                mediaUrl: "$mediaUrl",
+                // thumbnailUrl: "$",
+                // title: "$title",
+                // createdd:"$created",
+                // isPostLater: "$isPostLater",
+                description: "$description",
+                created: "$created",
+                // postedAt: "$postedAt",
+                createdAt: "$createdAt",
+                isLike:
+                {
+                    // $cond: { if: { "$eq": ["$likeData.userId", await appUtils.toObjectId(userId.userId)] }, then: true, else: false }
+                    $cond: { if: { "$eq": [{ $size: "$likeData" }, 0] }, then: true, else: false }
+                },
+                isComment: {
+                    $cond: { if: { "$eq": [{ $size: "$commentData" }, 0] }, then: false, else: true }
+                }
             }
-            if(!type) {
+            if (!type) {
                 project["_id"] = "$_idd"
             }
-            aggPipe.push({$project: project});
-            if(!type) {
-                result  = await this.aggregate('home', aggPipe, {})
-                if(result && result.length > 1) {
-                    result.sort(function(a, b){return b.type-a.type});
+            aggPipe.push({ $project: project });
+            if (!type) {
+                result = await this.aggregate('home', aggPipe, {})
+                if (result && result.length > 1) {
+                    result.sort(function (a, b) { return b.type - a.type });
                     result.reverse()
                 }
             } else {
-            aggPipe = [...aggPipe, ...await this.addSkipLimit(limit, pageNo)];
-            result = await this.aggregateWithPagination("home", aggPipe, limit, pageNo, true)
+                aggPipe = [...aggPipe, ...await this.addSkipLimit(limit, pageNo)];
+                result = await this.aggregateWithPagination("home", aggPipe, limit, pageNo, true)
             }
             return result
         } catch (error) {
