75e975e9ffdfb6da327be4b2629295882769fd1c
haider <haider.ali>
2020-08-14 18:13:53 +0530

    discover status updation

--- src/modules/discover/DiscoverValidator.ts
+++ src/modules/discover/DiscoverValidator.ts
@@ -10,8 +10,9 @@ let validateEditDiscover = Joi.object({
     discover_status: Joi.number().valid([
         config.CONSTANT.DISCOVER_STATUS.PENDING,
         config.CONSTANT.DISCOVER_STATUS.ACCEPT,
-        config.CONSTANT.DISCOVER_STATUS.REJECT
-    ]).description("1-pending,  2-accept, 3-reject").optional()
+        config.CONSTANT.DISCOVER_STATUS.REJECT,
+        config.CONSTANT.DISCOVER_STATUS.NO_ACTION
+    ]).description("1-pending,  2-accept, 3-reject, 4-no_action").optional()
 }).unknown()
 let validateEditDiscoverParams = Joi.object({
     discoverId: Joi.string().trim().regex(config.CONSTANT.REGEX.MONGO_ID).required(),
