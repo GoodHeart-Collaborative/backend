6ad298688db667da4bb0107b2e57eb1c6415be68
shubham <shubham.maheshwari@appinventiv.com>
2020-09-11 14:28:36 +0530

    update

--- src/modules/discover/DiscoverDao.ts
+++ src/modules/discover/DiscoverDao.ts
@@ -163,8 +163,8 @@ export class DiscoverDao extends BaseDao {
             }
             if (ShoutoutConnection) {
                 aggPipe = [...aggPipe, ...this.addSkipLimit(limit, pageNo)];
-                result = await this.aggregate('discover', aggPipe, {})
-                result = await this.aggregate('discover', aggPipe, {})
+                result = await this.aggregateWithPagination('discover', aggPipe)
+                // result = await this.aggregate('discover', aggPipe, {})
             } else {
                 aggPipe = [...aggPipe, ...this.addSkipLimit(limit, pageNo)];
                 result = await this.aggregateWithPagination('discover', aggPipe)
