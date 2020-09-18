8186d6fd969c056e3a1f0baf9c5aa1e85dee9e9d
shubham <shubham.maheshwari@appinventiv.com>
2020-09-18 17:23:00 +0530

    updayte

--- src/modules/eventInterest/eventInterestController.ts
+++ src/modules/eventInterest/eventInterestController.ts
@@ -40,7 +40,9 @@ class InterestController {
             if (!checkExist) {
                 criteria["created"] = new Date().getTime();
                 await eventInterestDao.insert('event_interest', criteria, {})
+                // const userEvent = await eventInterestDao.find('event_interest', { eventId: params.eventId, userId: params.userId }, {}, {}, {}, {}, {});
             }
+            const userEvent = await eventInterestDao.find('event_interest', { eventId: params.eventId, userId: params.userId }, {}, {}, {}, {}, {});
 
             if (type == config.CONSTANT.EVENT_INTEREST.GOING) {
                 data = await eventDao.findByIdAndUpdate('event', { _id: eventId }, { $inc: { goingCount: incOrDec } }, { new: true, lean: true })
@@ -48,19 +50,47 @@ class InterestController {
             if (type == config.CONSTANT.EVENT_INTEREST.INTEREST) {
                 data = await eventDao.findByIdAndUpdate('event', { _id: eventId }, { $inc: { interestCount: incOrDec } }, { new: true, lean: true })
             }
+
+            console.log('userEventuserEvent', userEvent);
+
+            data['isGoing'] = false;
+            data['isInterest'] = true;
+            if (userEvent.legth > 0) {
+                userEvent.map(data1 => {
+                    if (data1.type = 1) {
+                        data['isGoing'] = true;
+                    }
+                    if (data1.type = 2) {
+                        data['isInterest'] = true;
+                    }
+                })
+            }
+
+            return data;
             // INTEREST: 2
-            if (!checkExist && params.type === 2) {
-                data['isInterest'] = true;
-            }
-            if (checkExist && params.type === 2) {
-                data['isInterest'] = false;
-            }
-            if (!checkExist && params.type === 1) {
-                data['isGoing'] = true;
-            }
-            if (checkExist && params.type === 1) {
-                data['isGoing'] = false;
-            }
+            // if (!checkExist && params.type === 2) {
+            //     data['isInterest'] = true;
+            // }
+            // if (checkExist && params.type === 2) {
+            //     data['isInterest'] = false;
+            // }
+            // if (!checkExist && params.type === 1) {
+            //     data['isGoing'] = true;
+            // }
+            // if (checkExist && params.type === 1) {
+            //     data['isGoing'] = false;
+            // }
+            // if (data.type === 1) {
+            //     data['isGoing'] = true;
+            // } else if (data.type === 2) {
+            //     data['isIntereset'] = true;
+            // } else if (data.type === 1 && data.type === 2) {
+            //     data['isIntereset'] = true;
+            //     data['false'] = true;
+            // }
+
+
+
 
             return data;
         } catch (error) {
