4a80cf35e9f7d25beab53c435bdb9e4ffb4c48a3
shubham <shubham.maheshwari@appinventiv.com>
2020-09-25 11:07:09 +0530

    update

--- src/modules/discover/DiscoverController.ts
+++ src/modules/discover/DiscoverController.ts
@@ -112,7 +112,6 @@ class DiscoverController {
         try {
             let checkQuery: any = {}
             let status: any = {}
-            console.log('namenamenamenamename', name);
 
             checkQuery["$or"] = [{ userId: userId.userId, followerId: params.followerId }, { followerId: userId.userId, userId: params.followerId }]
             let checkDiscover = await discoverDao.checkDiscover(checkQuery)
