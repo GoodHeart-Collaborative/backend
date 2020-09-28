07e8e03386571e9aa7f9ba07b4b793db6a8b40e7
shubham <shubham.maheshwari@appinventiv.com>
2020-09-28 14:00:10 +0530

    update

--- src/modules/home/HomeController.ts
+++ src/modules/home/HomeController.ts
@@ -96,7 +96,7 @@ class HomeController {
                     homeData: responseData,
                     isGratitudeJournalFilled: isGratitudeFilled,
                     subscriptionData: {
-                        isSubscribed: (userId.tokenData.planType !== config.CONSTANT.USER_SUBSCRIPTION_PLAN.NONE.value) ? true : false,
+                        isSubscribed: true, // (userId.tokenData.planType !== config.CONSTANT.USER_SUBSCRIPTION_PLAN.NONE.value) ? true : false,
                         subscriptionType: userId.tokenData.subscriptionType,
                         subscriptionEndDate: userId.tokenData.subscriptionEndDate,
                         unreadNotificationCount: notificationCount
