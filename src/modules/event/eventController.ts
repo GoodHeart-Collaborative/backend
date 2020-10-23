a9ab3f41b014fec6a3376b4d74391d646fd0ad48
shubham <shubham.maheshwari@appinventiv.com>
2020-10-23 12:55:34 +0530

    update

--- src/modules/event/eventController.ts
+++ src/modules/event/eventController.ts
@@ -869,6 +869,7 @@ class EventController {
                     eventCategoryName: 1,
                     title: 1,
                     address: 1,
+                    status: 1,
                     friends: [],
                     // hostUser: 1,
                     hostUser: {
@@ -887,6 +888,7 @@ class EventController {
             })
 
             const data = await eventDao.aggregate('event', aggPipe, {})
+            console.log('datadatadata', data);
 
             // for the deeplink case
             if (payload.eventId && data[0] && data[0].endDate < new Date().getTime()) {
@@ -899,8 +901,10 @@ class EventController {
                 data[0].eventStatusMessage = eventConstant.MESSAGES.EVENT_BLOCKED_DELETE;
                 data[0].eventStatus = eventConstant.MESSAGES.EVENT_BLOCKED;
             }
-            else {
-                data[0].eventStatus = eventConstant.MESSAGES.EVENT_ACTIVE;
+            if (payload.eventId && data[0] && data[0].status === config.CONSTANT.STATUS.ACTIVE && data[0].endDate > new Date().getTime()) {
+                // data[0].eventStatusMessage = eventConstant.MESSAGES.
+                data[0].eventStatusMessage = eventConstant.MESSAGES.EVENT_ACTIVE;
+                // data[0].eventStatus = eventConstant.MESSAGES.EVENT_EXPIRED_MESSAGE ;
             }
             return data[0] ? data[0] : {};
         } catch (error) {
