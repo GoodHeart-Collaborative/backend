9d2c85578ac9f27aa0029825a65daada3d5adca1
shubham <shubham.maheshwari@appinventiv.com>
2021-03-19 01:33:04 +0530

    updayte

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 cd /var/www/html/goodheart_api
 #NODE_ENV=beta pm2 start ./build/server.js --name Women_Beta
 pm2 delete all
-NODE_ENV=beta pm2 start ./build/server.js --name women_beta
+NODE_ENV=production pm2 start ./build/server.js --name women_prod
