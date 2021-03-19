f59d3a70392efb72fc720ee4c441b666872edcc7
shubham <shubham.maheshwari@appinventiv.com>
2021-03-19 11:49:41 +0530

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /var/www/html/goodheart_api
 #NODE_ENV=beta pm2 start ./build/server.js --name Women_Beta
+aws s3 cp s3://womenheart/env.production /var/www/html/goodheart_api
+cp -r env.production .env.production
 pm2 delete all
 NODE_ENV=production pm2 start ./build/server.js --name women_prod
+
+
